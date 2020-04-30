5490240f11330d10d1e20f07d784d0cef1c081ba
Trang Dao <trang.dao909@tcu.edu>
2020-04-30 17:05:01 -0500

    webpack_demo_1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
  import _ from 'lodash';
+ import './style.css';
+ import Cat from './cat.jpg';
+ import Data from './data.xml';
+
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
+    // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = Cat;
+
+   element.appendChild(myIcon);
+
+   console.log(Data);
+
     return element;
   }
   
